6a319678f5d234109c081bf3004b23d73321d3fb
de119 <de119@skbl-422-013.ph.ic.ac.uk>
2022-03-10 10:32:43 +0000

    Baby steps...

--- rotate.s
+++ rotate.s
@@ -25,12 +25,12 @@ Rotate_Servo:
 	
 	movwf CCPR1L,A ; ?
 	movwf CCPR1H,A ; ?
-	bcf TRISC,CCP1,A ; configure CCP1 pin for output
+	bcf TRISC,3,A ; configure CCP1 pin for output
 	movlw 0x81; enable Timer3 in 16-bit mode and use
 	movwf T3CON,A ; Timer2 as time base for PWM1 thru PWM5
 	clrf TMR2,A ; force TMR2 to count from 0
 	movlw 00000101B ; enable Timer2 and set its prescaler 
-	movwf T2CON A ; ?
+	movwf T2CON, A ; ?
 	movlw 0x0C ; enable CCP1 PWM mode
 	movwf CCP1CON,A ;
 
