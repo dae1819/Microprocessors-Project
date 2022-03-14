e231bedbe298547875dacff91e98d193fbdf18bb
de119 <de119@skbl-422-013.ph.ic.ac.uk>
2022-03-14 14:45:54 +0000

    delay 10us, interfaced LDRs

--- main.s
+++ main.s
@@ -3,9 +3,7 @@
 extrn	DAC_Setup, DAC_Int_Hi
 extrn	ADC_Setup, ADC_Read
 
-psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable    
-    
+
     
 psect	code, abs
 rst:	org	0x0000	; reset vector
@@ -15,13 +13,18 @@ int_hi:	org	0x0008	; high vector, no low vector
 	goto	DAC_Int_Hi
 	
 start:	
+	
+	
+	
 	call	ADC_Setup	; setup ADC
 	
 	
+	movlw	15 ;period is 15 x 100us = 1.5ms
 	call	DAC_Setup
+	
 	goto	$	; Sit in infinite loop
 
-
+	
 
 	
 	
