bcc784571bff463b238ad2703bd9d04fa48d6673
de119 <de119@skbl-422-013.ph.ic.ac.uk>
2022-03-14 12:06:11 +0000

    delay 10us, interfaced LDRs

--- DAC_Interrupt.s
+++ DAC_Interrupt.s
@@ -7,16 +7,16 @@ psect	dac_code, class=CODE
 DAC_Int_Hi:	
 	btfss	TMR0IF		; check that this is timer0 interrupt
 	retfie	f		; if not then return
+	
+	;check counter number, specify if 1 or 0 depending on count
+	
+	
+	
 	comf	LATJ, F, A	; bit flip PORTD
 	bcf	TMR0IF		; clear interrupt flag
 	
-;	movlw	0x01
-;	movwf	TMR0H
-;	movlw	0x00
-;	movwf	TMR0L
-;	
-;	movlw	11001000B	; Set timer0 to 16-bit, Fosc/4/256
-;	movwf	T0CON, A	; = approx 5ms rollover
+	movlw	0x60
+	movwf	TMR0
 	
 	retfie	f		; fast return from interrupt
 
@@ -24,14 +24,15 @@ DAC_Setup:
 	clrf	TRISJ, A	; Set PORTD as all outputs
 	clrf	LATJ, A		; Clear PORTD outputs
 	
-	movlw	0x01
-	movwf	TMR0H
-	movlw	0x00
-	movwf	TMR0L
+	
+	;Dont want to count from 0 i.e want 16us -> 10us
+;	;Corresponds to counting from 96 (0x60) -> 2^8 instead of 0 -> 2^8
+	movlw	0x60
+	movwf	TMR0
 	
 	
-	movlw	11001000B	; Set timer0 to 16-bit, Fosc/4/256
-	movwf	T0CON, A	; = 62.5KHz clock rate, approx 1sec rollover
+	movlw	11001000B	; Set timer0 to 8-bit, Fosc/4 = Fclock
+	movwf	T0CON, A	
 	bsf	TMR0IE		; Enable timer0 interrupt
 	bsf	GIE		; Enable all interrupts
 	return
