48e6caab362e6bafc5372939603d0f97f560e59a
de119 <de119@skbl-422-013.ph.ic.ac.uk>
2022-03-14 14:45:48 +0000

    delay 10us, interfaced LDRs

--- DAC_Interrupt.s
+++ DAC_Interrupt.s
@@ -1,6 +1,12 @@
 #include <xc.inc>
 	
 global	DAC_Setup, DAC_Int_Hi
+
+psect	udata_acs   ; reserve data space in access ram
+counter:    ds 1    ;num of 100us in period   
+pulse_width:	ds 1  ;num of 100us in high pulse  
+period:		ds 1    
+    
     
 psect	dac_code, class=CODE
 	
@@ -8,30 +14,60 @@ DAC_Int_Hi:
 	btfss	TMR0IF		; check that this is timer0 interrupt
 	retfie	f		; if not then return
 	
-	;check counter number, specify if 1 or 0 depending on count
+	
+	;check counter value. If more than period reset to 0
+	movf	period,W
+	cpfslt	counter
+	clrf	counter
 	
 	
 	
-	comf	LATJ, F, A	; bit flip PORTD
-	bcf	TMR0IF		; clear interrupt flag
+	;compare counter to pulse_width, specify if 1 or 0 
+	movf	pulse_width,W
+	cpfslt	counter
+	movlw	00000000B
 	
-	movlw	0x60
+	movf	pulse_width,W
+	cpfsgt	counter
+	movlw	11111111B
+	
+	
+	movwf	LATJ
+	
+	;Increment counter
+	incf	counter
+	
+	; clear interrupt flag
+	bcf	TMR0IF		
+	
+	movlw	0x38
 	movwf	TMR0
 	
 	retfie	f		; fast return from interrupt
 
 DAC_Setup:
+    
+	movwf	pulse_width
+    
+	movlw	200
+	movwf	period	;20ms = 200 x 100us
+	
+	movlw	0
+	movwf	counter
+	
+	
+    
 	clrf	TRISJ, A	; Set PORTD as all outputs
 	clrf	LATJ, A		; Clear PORTD outputs
 	
 	
-	;Dont want to count from 0 i.e want 16us -> 10us
-;	;Corresponds to counting from 96 (0x60) -> 2^8 instead of 0 -> 2^8
-	movlw	0x60
+	;Dont want to count from 0 i.e want 128us -> 100us
+;	;Corresponds to counting from 56 (0x38) -> 2^8 instead of 0 -> 2^8
+	movlw	0x38
 	movwf	TMR0
 	
 	
-	movlw	11001000B	; Set timer0 to 8-bit, Fosc/4 = Fclock
+	movlw	11000010B	; Set timer0 to 8-bit, 1:8 prescaler
 	movwf	T0CON, A	
 	bsf	TMR0IE		; Enable timer0 interrupt
 	bsf	GIE		; Enable all interrupts
