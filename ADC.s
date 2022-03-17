39549640998a6485488f498004e86320da89d98c
de119 <de119@skbl-422-013.ph.ic.ac.uk>
2022-03-17 12:07:41 +0000

    LDRs now able to be read. Need to fix equal to comparison!

--- ADC.s
+++ ADC.s
@@ -21,10 +21,15 @@ ADC_Setup0:
 ADC_Setup1:
 	bsf	TRISA, PORTA_RA1_POSN, A  ; pin RA1==AN0 input; CHANGE FOR OTHER PINS!
 	movlb	0x0f
-	bsf	ANSEL0	    ; set AN0 to analog
+	bsf	ANSEL1	    ; set AN0 to analog
 	movlb	0x00
-	movlw   0x01	    ; select AN0 for measurement
+	
+	;NEED TO CHANGE THIS...
+	movlw   00000101B	    ; select AN0 for measurement
 	movwf   ADCON0, A   ; and turn ADC on
+	
+	
+	;THIS BIT CAN BE RE-USED
 	movlw   0x30	    ; Select 4.096V positive reference
 	movwf   ADCON1,	A   ; 0V for -ve reference and -ve input
 	movlw   0xF6	    ; Right justified output
@@ -34,22 +39,25 @@ ADC_Setup1:
 ADC_Setup2:
 	bsf	TRISA, PORTA_RA2_POSN, A  ; pin RA2==AN0 input; CHANGE FOR OTHER PINS!
 	movlb	0x0f
-	bsf	ANSEL0	    ; set AN0 to analog
+	bsf	ANSEL2	    ; set AN0 to analog
 	movlb	0x00
-	movlw   0x01	    ; select AN0 for measurement
+	
+	movlw   00001001B	    ; select AN0 for measurement
 	movwf   ADCON0, A   ; and turn ADC on
+	
 	movlw   0x30	    ; Select 4.096V positive reference
 	movwf   ADCON1,	A   ; 0V for -ve reference and -ve input
 	movlw   0xF6	    ; Right justified output
 	movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
 	return
+
 	
 ADC_Setup3:
 	bsf	TRISA, PORTA_RA3_POSN, A  ; pin RA0==AN0 input; CHANGE FOR OTHER PINS!
 	movlb	0x0f
-	bsf	ANSEL0	    ; set AN0 to analog
+	bsf	ANSEL3	    ; set AN0 to analog
 	movlb	0x00
-	movlw   0x01	    ; select AN0 for measurement
+	movlw   00010001B	    ; select AN0 for measurement
 	movwf   ADCON0, A   ; and turn ADC on
 	movlw   0x30	    ; Select 4.096V positive reference
 	movwf   ADCON1,	A   ; 0V for -ve reference and -ve input
